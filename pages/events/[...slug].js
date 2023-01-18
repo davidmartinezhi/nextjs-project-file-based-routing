150696a865b86aa518d4df3af1419d63f78666b6
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-01-18 16:55:50 -0600

    added error alert component for invalida filters

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -3,6 +3,8 @@ import { useRouter } from "next/router";
 import { getFilteredEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/results-title/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
 
 function FilteredEventsPage() {
   const router = useRouter();
@@ -30,9 +32,15 @@ function FilteredEventsPage() {
     numMonth > 12
   ) {
     return (
-      <div>
-        <h1>Invalid filter. Please adjust your values!</h1>
-      </div>
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
     );
   }
 
@@ -44,7 +52,16 @@ function FilteredEventsPage() {
 
   //Check if filtered events are falsy or empty array
   if (!filteredEvents || filteredEvents.length == 0) {
-    return <p>No events found for the chosen filter :/</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found for the chosen filter :/</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
   //Date passed to result title
