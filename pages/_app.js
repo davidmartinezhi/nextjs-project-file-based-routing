ffed2ff462374b39198d83b5479fcfdab7e739d7
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-01-18 13:21:15 -0600

    added layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  {
+    /* Root component where pages will be rendered in */
+  }
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
