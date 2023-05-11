f9346a40c8545faf204b847f0b934565530eeed8
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-05-11 12:39:07 -0600

    initila file baseed routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,10 @@
-import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   {
     /* Root component where pages will be rendered in */
   }
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
