06f4915dd5afcbfeba011e922db7493aff6e0fa3
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-05-11 13:38:52 -0600

    added icons

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
 import Button from "../ui/button";
+
 import classes from "./EventItem.module.css";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -20,15 +24,25 @@ function EventItem(props) {
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
+
           <div className={classes.date}>
+            <DateIcon />
+
             <time>{humanReadableDate}</time>
           </div>
+
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
