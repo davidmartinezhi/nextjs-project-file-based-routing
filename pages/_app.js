916660b68a692d5ffabb7970784b4cd31297cc7f
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-05-15 11:24:07 -0600

    Layout wrapper and event page details

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
+import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   {
     /* Root component where pages will be rendered in */
   }
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
