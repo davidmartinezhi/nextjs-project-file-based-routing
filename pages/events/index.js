c57ff77019d748cd46fb88d01955da114b0550a8
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-05-15 12:16:10 -0600

    navigating to filtered events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,3 +1,5 @@
+import { useRouter } from "next/router";
+
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/events-search";
@@ -6,10 +8,16 @@ import { Fragment } from "react";
 
 function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   );
