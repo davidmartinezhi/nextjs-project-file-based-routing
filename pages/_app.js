8ca4684c7752a908b8d639c69a64aeae73310124
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-05-25 12:49:40 -0600

    added global head configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,22 @@
+import Head from "next/head";
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   {
-    /* Root component where pages will be rendered in */
+    /* 
+    -Root component where pages will be rendered in 
+    -Renders for every page that is displayed
+    */
   }
   return (
     <Layout>
+      <Head>
+        {/* 
+        This head is applied for all pages
+        */}
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
