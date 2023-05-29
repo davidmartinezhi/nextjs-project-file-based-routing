3fbdfe29d2563dda304f27a1f771ca42ced2382f
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-05-29 11:31:21 -0600

    running mongodb queries from inside api routes

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     //get user email
     const userEmail = req.body.email;
@@ -9,7 +11,20 @@ function handler(req, res) {
       return;
     }
 
-    console.log(userEmail);
+    //connect to client server
+    const client = await MongoClient.connect(
+      "mongodb+srv://dbDavid:dMfjJ0cXrwpOlJEO@atlascluster.kjefvkf.mongodb.net/newsletter?retryWrites=true&w=majority"
+    );
+
+    //get access to db
+    const db = client.db();
+
+    //insert document (query) to email collection
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    //disconnect from client sever
+    client.close();
+
     res.status(201).json({ message: "Signed Up" });
   }
 }
