d4ef47a0bc67a846c78b0774fb7fd8c0e7a5ccee
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-05-31 12:12:20 -0600

    added context state

--- store/notification-context.js
+++ store/notification-context.js
@@ -3,17 +3,38 @@ import { createContext, useState } from "react";
 //we get can component out of this code
 const NotificationContext = createContext({
   notification: null, // {title, message status}
-  showNotification: function () {},
+  showNotification: function (notificationData) {},
   hideNotification: function () {},
 });
 
 //component that manages all component state
 export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification({
+      title: notificationData.title,
+      message: notificationData.message,
+      status: notificationData.status,
+    });
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  //bundle data and functions for distribution
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
   return (
-    <NotificationContext.Provider>
+    <NotificationContext.Provider value={context}>
       {props.children}
     </NotificationContext.Provider>
   );
 }
 
-export default notificationContext;
+export default NotificationContext;
